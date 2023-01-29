7c6ebbe465a482014ee33451b4d0b4f5d8e65bc5
yagocrispim0 <yagocrispimgomes@gmail.com>
2023-01-29 14:23:40 -0300

    Async tasks

--- SalesWebMvc/Services/DepartmentService.cs
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -1,5 +1,9 @@
 ﻿using SalesWebMvc.Data;
 using SalesWebMvc.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services
 {
@@ -12,9 +16,9 @@ namespace SalesWebMvc.Services
             _context = context;
         }   
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(d => d.Name).ToList();
+            return await _context.Department.OrderBy(d => d.Name).ToListAsync();
         }
     }
 }
