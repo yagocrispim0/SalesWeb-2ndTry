29e9d04d9d544e8e0ec260398970f91d94303bd4
yagocrispim0 <yagocrispimgomes@gmail.com>
2023-01-29 15:55:29 -0300

    final

--- SalesWebMvc/Controllers/SalesRecordsController.cs
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -37,9 +37,24 @@ namespace SalesWebMvc.Controllers
             return View(result);
         }
 
-        public IActionResult GroupingSearch()
+        public async Task<IActionResult> GroupingSearchAsync(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            //passar a data para o campo após a pesquisa
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
+
+            var result = await _salesRecordService.FindByDateGroupingAsync(minDate, maxDate);
+            return View(result);
         }
     }
 }
--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -1,11 +1,14 @@
 ﻿using SalesWebMvc.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMvc.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")] // Opção: Deixar horário para que se possa ter esse controle.
         public DateTime Date { get; set; }
+        [DisplayFormat(DataFormatString ="{0:F2}")]
         public Double Amount { get; set; }
         public SaleStatus SaleStatus { get; set; }
         public Seller Seller { get; set; }
